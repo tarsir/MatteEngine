b99baae3b4fb6974220e71282887b18d80c4f1be
Stephen Hara <sthara216@gmail.com>
2018-10-10 23:17:13 +0900

    add basic animation component implementation

---
+++ MatteEngine/AnimationComponent.cpp
@@ -0,0 +1,41 @@
+#include "AnimationComponent.h"
+
+auto animation_c_logger = spdlog::stdout_color_mt("AnimationComponent.h");
+
+AnimationInfo AnimationComponent::get_current_animation_description() {
+	return this->animation_sequence[this->current_animation];
+}
+
+int AnimationComponent::advance_animation() {
+	if (this->is_in_motion) {
+		this->current_animation = (this->current_animation == this->animation_sequence.size() - 1)
+			? 0
+			: this->current_animation + 1;
+	}
+	return this->current_animation;
+}
+
+void AnimationComponent::stop_motion() {
+	this->is_in_motion = false;
+}
+
+void AnimationComponent::start_motion() {
+	this->is_in_motion = true;
+}
+
+bool AnimationComponent::is_moving() {
+	return this->is_in_motion;
+}
+
+std::string AnimationComponent::get_neutral_description_for_direction(Facing direction) {
+	auto is_neutral_for_direction = [direction](AnimationInfo anim_info) {
+		return anim_info.is_neutral && anim_info.animation_direction == direction;
+	};
+	auto it = std::find_if(this->animation_sequence.begin(), this->animation_sequence.end(), is_neutral_for_direction);
+	if (it != this->animation_sequence.end()) {
+		return it->description;
+	}
+
+	animation_c_logger->error("Could not find matching neutral for direction: {}", direction);
+	return NULL;
+}
--- MatteEngine/AnimationComponent.h
+++ MatteEngine/AnimationComponent.h
@@ -8,7 +8,6 @@
 #include "ComponentList.h"
 #include "spdlog\spdlog.h"
 
-//auto animation_c_logger = spdlog::stdout_color_mt("AnimationComponent.h");
 
 enum Facing {
 	NORTH = 99000,
@@ -17,6 +16,9 @@ enum Facing {
 	WEST
 };
 
+/*
+	What does this do?
+*/
 struct AnimationFrame {
 
 };
@@ -40,39 +42,10 @@ public:
 		this->current_animation = 0;
 		this->is_in_motion = false;
 	}
-
-	AnimationInfo get_current_animation_description() {
-		return this->animation_sequence[this->current_animation];
-	}
-
-	int advance_animation() {
-		this->current_animation = this->current_animation == this->animation_sequence.size() ? 0 : this->current_animation + 1;
-		return this->current_animation;
-	}
-
-	void stop_motion() {
-		this->is_in_motion = false;
-	}
-
-	void start_motion() {
-		this->is_in_motion = true;
-	}
-
-	bool is_moving() {
-		return this->is_in_motion;
-	}
-
-	std::string get_neutral_description_for_direction(Facing direction) {
-		auto is_neutral_for_direction = [direction](AnimationInfo anim_info) {
-			return anim_info.is_neutral && anim_info.animation_direction == direction;
-		};
-		auto it = std::find_if(this->animation_sequence.begin(), this->animation_sequence.end(), is_neutral_for_direction);
-		if (it != this->animation_sequence.end()) {
-			return it->description;
-		}
-
-		//TODO: move this to .cpp file
-		//animation_c_logger->error("Could not find matching neutral for direction: {}", direction);
-		return NULL;
-	}
+	AnimationInfo get_current_animation_description();
+	int advance_animation();
+	void stop_motion();
+	void start_motion();
+	bool is_moving();
+	std::string get_neutral_description_for_direction(Facing direction);
 };
